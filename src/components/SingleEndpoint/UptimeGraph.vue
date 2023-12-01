8157649452d571b50f82e65a9600268552e02ff6
Alex Krumov <alex.krumov@bundlearrows.com>
2023-12-01 18:09:25 +0200

    Remove unsued component

--- src/components/SingleEndpoint/UptimeGraph.vue
+++ src/components/SingleEndpoint/UptimeGraph.vue
@@ -68,10 +68,10 @@ export default {
     },
     getUTCHourFromTimestamp(unixTimestamp) {
       const date = new Date(unixTimestamp * 1000);
-
+      console.log(date)
       const year = date.getFullYear();
       const month = String(date.getMonth() + 1).padStart(2, '0'); // months are 0-indexed
-      const day = String(date.getDate()).padStart(2, '0');
+      const day = String(date.getDay()).padStart(2, '0');
       const utcHour = date.getHours();
 
       return `${year}-${month}-${day} ${utcHour}:00:00`;
--- src/utils/sharedFunctions.js
+++ src/utils/sharedFunctions.js
@@ -4,9 +4,9 @@ export function unixTimestampToFormattedString(timestamp) {
   const year = date.getFullYear();
   const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Month is 0-based, so add 1
   const day = date.getDate().toString().padStart(2, '0');
-  const hours = date.getUTCHours().toString().padStart(2, '0');
-  const minutes = date.getUTCMinutes().toString().padStart(2, '0');
-  const seconds = date.getUTCSeconds().toString().padStart(2, '0');
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
 
   const formattedString = `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
   return formattedString;
