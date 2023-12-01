d9107057f5bce0c5c4b98d79c981f1bb432ecbf7
Alex Krumov <alex.krumov@bundlearrows.com>
2023-12-01 17:48:21 +0200

    Remove unsued component

--- src/components/SingleEndpoint/UptimeGraph.vue
+++ src/components/SingleEndpoint/UptimeGraph.vue
@@ -69,10 +69,10 @@ export default {
     getUTCHourFromTimestamp(unixTimestamp) {
       const date = new Date(unixTimestamp * 1000);
 
-      const year = date.getUTCFullYear();
-      const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // months are 0-indexed
-      const day = String(date.getUTCDate()).padStart(2, '0');
-      const utcHour = date.getUTCHours();
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0'); // months are 0-indexed
+      const day = String(date.getDate()).padStart(2, '0');
+      const utcHour = date.getHours();
 
       return `${year}-${month}-${day} ${utcHour}:00:00`;
     },
