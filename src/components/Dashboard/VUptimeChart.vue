90a03054b3ccde2b9472843f04b70697a517965d
Alex Krumov <alex.krumov@bundlearrows.com>
2024-04-03 22:01:52 +0300

    Remove topheader background

--- src/components/Dashboard/VUptimeChart.vue
+++ src/components/Dashboard/VUptimeChart.vue
@@ -28,13 +28,27 @@ export default {
 
       return date.getTime();
     },
+    formatDate(timestamp) {
+      var dateObject = new Date(timestamp * 1000);
+      var month = new Intl.DateTimeFormat('en-US', { month: 'long' }).format(dateObject);
+      var day = dateObject.getDate();
+      var year = dateObject.getFullYear();
+      var hour = dateObject.getHours().toString().padStart(2, '0');
+      var minute = dateObject.getMinutes().toString().padStart(2, '0');
+      return `${month} ${day},${year} - ${hour}:${minute} h`;
+    }
   },
 };
 </script>
 
 <template>
   <ul class="uptime-graph">
-    <li v-for="item in data" :key="item" :class="`uptime-item ${item.status} ${item?.is_active}`" :tooltip-text="getEndpointTooltip(item)" tooltip-position="Top" />
+    <li v-for="item in data" :key="item" :class="`uptime-item ${item.status} ${item?.is_active}`">
+      <div>
+        <span>{{ formatDate(item.created_at) }}</span>
+        <span><span :status="item.status"></span>{{ item.status }} (100%)</span>
+      </div>
+    </li>
   </ul>
 </template>
 
@@ -44,6 +58,8 @@ export default {
   width: 100%;
   height: 100%;
   gap: 4px;
+  z-index: 2;
+  cursor: pointer;
 }
 
 .uptime-graph li:first-child {
@@ -58,19 +74,41 @@ export default {
   padding: 0;
   width: -webkit-fill-available;
   cursor: pointer;
+  position: relative;
+  transition: background-color 300ms ease-in-out;
 }
 
-.uptime-graph li.uptime-item.healthy{
+.uptime-graph li.uptime-item.healthy,
+.uptime-graph li:hover div span[status="healthy"]{
   background-color: var(--green-500);
 }
-.uptime-graph li.uptime-item.degraded{
+.uptime-graph li.uptime-item.healthy:hover {
+  background-color: #187c41;
+}
+
+.uptime-graph li.uptime-item.degraded,
+.uptime-graph li:hover div span[status="degraded"]{
   background-color: var(--red-500);
 }
-.uptime-graph li.uptime-item.unhealthy{
+.uptime-graph li.uptime-item.degraded:hover {
+  background-color: #932f32;
+}
+
+.uptime-graph li.uptime-item.unhealthy,
+.uptime-graph li:hover div span[status="unhealthy"]{
   background-color: yellow;
 }
-.uptime-graph li.uptime-item.nodata{
+.uptime-graph li.uptime-item.unhealthy:hover {
+  background-color: #9c8c34;
+}
+
+
+.uptime-graph li.uptime-item.nodata,
+.uptime-graph li:hover div span[status="nodata"]{
   background-color: var(--bar-chart-bg);
+}
+.uptime-graph li.uptime-item.nodata:hover {
+  background-color: #10171f;
 }
 
 .uptime-graph li.uptime-item.true {
@@ -83,4 +121,53 @@ export default {
 .uptime-graph [tooltip-text]::after {
   min-width: 150px;
 }
+
+.uptime-graph li div {
+  min-width: 178px;
+  position: absolute;
+  background-color: var(--gray-scale-6);
+  padding: 8px 12px;
+  display: none;
+  border-radius: 2px;
+  top:-68px;
+  left:-64px;
+  right: 0;
+}
+
+
+.uptime-graph li div::before {
+  content: url("data:image/svg+xml,%3Csvg width='7' height='8' viewBox='0 0 7 8' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M7 8L7 0L0.788855 3.10557C0.0518064 3.4741 0.0518053 4.5259 0.788854 4.89443L7 8Z' fill='%23414B55'/%3E%3C/svg%3E");
+  position: absolute;
+  bottom: -13px;
+  left: 50%;
+  transform: rotate(270deg);
+}
+
+.uptime-graph li:hover div {
+  display: inherit;
+  font-size: 13px;
+  font-weight: 400;
+  line-height: 20px;
+  display: flex;
+  gap: 5px;
+  flex-flow: column;
+}
+
+.uptime-graph li:hover div span:first-child {
+  color: var(--gray-scale-4)
+}
+.uptime-graph li:hover div span:last-child {
+  color: white;
+  text-transform: capitalize;
+  display: flex;
+  gap: 8px;
+  align-items: center;
+}
+
+.uptime-graph li:hover div span[status] {
+  width: 10px;
+  height: 10px;
+  border-radius: 2px;
+}
+
 </style>
